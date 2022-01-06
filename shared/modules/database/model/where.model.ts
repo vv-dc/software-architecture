cee8befd85f6a81643a50c309adb1a3bef61871f
vvkin <vkincur@gmail.com>
2022-01-07 01:17:12 +0200

    Implement shared functionality

---
+++ shared/modules/database/model/where.model.ts
@@ -0,0 +1,6 @@
+import { StringQuery } from './string-query.model';
+import { NumberQuery } from './number-query.model';
+
+export type WhereQuery<T> = {
+  [key in keyof T]?: StringQuery | NumberQuery;
+};
